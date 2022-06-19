056bbc72bbb720b0042a9b0328d9639010ef3ad5
jjohns49 <jjohns49@villanova.edu>
2022-06-19 18:38:03 -0400

    minor changes

--- tvd_web/src/App.vue
+++ tvd_web/src/App.vue
@@ -9,7 +9,9 @@
     <router-link to="/contact">Contact</router-link>
     <!--Need to add more router links to the different pages -->
   </nav>
+<div id = "router">
   <router-view/>
+</div>
 </template>
 
 <style>
@@ -19,6 +21,10 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
+}
+
+#router{
+  /* Need something here to spice it up*/
 }
 
 body{
@@ -37,7 +43,7 @@ nav {
 nav a {
   font-size: 150%;
   font-weight: bold;
-  padding: 1em;
+  padding: 3em;
   color:rgba(255, 228, 196, 0.822);
 }
 
--- tvd_web/src/views/HomeView.vue
+++ tvd_web/src/views/HomeView.vue
@@ -2,6 +2,14 @@
   <div class="home">
     <TVDHome/>
   </div>
+  <div class = "paragraph">
+    <p>
+      Since 1953, Talcott View Development has delivered on providing quality services for Connecticut
+    </p>
+  </div>
+  <div class = "Rosewood">
+    <img src = "../assets/12_Rosewood_1964.jpg">
+  </div>
 </template>
 
 <script>
@@ -15,3 +23,16 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.paragraph p{
+  padding: 1em;
+  color:rgba(255, 228, 196, 0.822);
+  float: left;
+}
+
+.Rosewood img{
+  width: 35%;
+  padding: 1em;
+}
+</style>
