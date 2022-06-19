2189c8d8fc82c06fbc8dbe315c3340803b035d99
jjohns49 <jjohns49@villanova.edu>
2022-06-19 19:42:50 -0400

    Added tailwind to the project and tried to recreate what I had before

--- tvd_web/src/App.vue
+++ tvd_web/src/App.vue
@@ -1,53 +1,17 @@
 <template>
-  <div id = "logo">
-    <img src = "../src/assets/tvd_logo.png">
+<div class = "bg-black">
+  <div class = "flex items-center justify-center">
+    <img class = "w-1/4" src = "../src/assets/tvd_logo.png">
   </div>
-  <nav>
-    <router-link to="/">Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-link to="/services">Services</router-link>
-    <router-link to="/contact">Contact</router-link>
+  <nav class = "flex items-center justify-center space-x-10">
+    <router-link class = "text-slate-600 hover:text-stone-400" to="/">Home</router-link>
+    <router-link class = "text-slate-600 hover:text-stone-400" to="/about">About</router-link>
+    <router-link  class = "text-slate-600 hover:text-stone-400" to="/services">Services</router-link>
+    <router-link class = "text-slate-600 hover:text-stone-400" to="/contact">Contact</router-link>
     <!--Need to add more router links to the different pages -->
   </nav>
-<div id = "router">
+<div class="bg-gray-700">
   <router-view/>
 </div>
+</div>
 </template>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#router{
-  /* Need something here to spice it up*/
-}
-
-body{
-  background: black;
-}
-
-#logo img{
-  max-height: auto;
-  width: 25%;
-}
-
-nav {
-  padding: 30px;
-}
-
-nav a {
-  font-size: 150%;
-  font-weight: bold;
-  padding: 3em;
-  color:rgba(255, 228, 196, 0.822);
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
--- tvd_web/src/views/HomeView.vue
+++ tvd_web/src/views/HomeView.vue
@@ -1,38 +1,10 @@
 <template>
-  <div class="home">
-    <TVDHome/>
-  </div>
-  <div class = "paragraph">
+  <div class = "float-left">
     <p>
       Since 1953, Talcott View Development has delivered on providing quality services for Connecticut
     </p>
   </div>
-  <div class = "Rosewood">
+  <div class = " w-1/4 p-1.0">
     <img src = "../assets/12_Rosewood_1964.jpg">
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import TVDHome from '../components/TVDHome.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    TVDHome
-  }
-}
-</script>
-
-<style scoped>
-.paragraph p{
-  padding: 1em;
-  color:rgba(255, 228, 196, 0.822);
-  float: left;
-}
-
-.Rosewood img{
-  width: 35%;
-  padding: 1em;
-}
-</style>
